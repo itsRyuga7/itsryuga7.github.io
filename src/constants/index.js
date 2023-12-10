d5fb5764815c3e94e87b749512ef8a9f8dc5be95
itsRyuga7 <131341759+itsRyuga7@users.noreply.github.com>
2023-12-11 01:05:09 +0530

    File upload

---
+++ src/constants/index.js
@@ -0,0 +1,151 @@
+import {
+    contact,
+    css,
+    estate,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    motion,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    sass,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    python,
+    c,
+    discord
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: python,
+        name: "Python",
+        type: "Programming Language",
+    },
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: c,
+        name: "C++",
+        type: "Programming Language",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/itsRyuga7',
+    },
+    {
+        name: 'Discord',
+        iconUrl: discord,
+        link: 'https://discord.com',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Amazon Price Tracker',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/adrianhajdin/pricewise',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Google Maps Simulator',
+        description: 'The Google Map Simulator is a tool designed to help users better understand and visualize how google maps and other navigation software work.',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-pink',
+        name: 'Drago (AI-Assistant)',
+        description: 'AI assistant that assists me in every form and can speak and listen to my commands while having an intelligent conversational ability. It can access WhatsApp and Gmail, set reminders and appointments, play music, answer questions related to general knowledge or specific topics, and give weather updates and news briefings.',
+        link: 'https://github.com',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Object Detection App',
+        description: 'Object Detection App is a tool accurately detects and track specific objects in real-time. It can detect a wide range of objects in various environments, making it ideal for use in surveillance, security, and monitoring applications.',
+        link: 'https://github.com/',
+    }
+];
